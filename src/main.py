1e545e31cb7058f352ec58680a4304b3e1bc1751
almaz_macbook <almazgimaev@gmail.com>
2023-04-06 16:44:21 +0300

    refactor script

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ if workspace is None:
 
 ################################    Part 1    ######################################
 ###################    create empty project and dataset    #########################
-################################    ------    ######################################
+############################     upload video    ###################################
 
 # create empty project and dataset on server
 project = api.project.create(
@@ -32,24 +32,28 @@ project = api.project.create(
 dataset = api.dataset.create(project.id, name="orange & kiwi")
 print(f"Project has been sucessfully created, id={project.id}")
 
-# create ObjClasses and VideoObjects
+
+# upload video to the dataset on server
+video_path = "data/orange_kiwi.mp4"
+video_name = sly.fs.get_file_name_with_ext(video_path)
+video_info = api.video.upload_path(dataset.id, video_name, video_path)
+print(f"Video has been sucessfully uploaded, id={video_info.id}")
+
+
+# create annotation classes
 kiwi_obj_cls = sly.ObjClass("kiwi", sly.Rectangle, color=[0, 0, 255])
 orange_obj_cls = sly.ObjClass("orange", sly.Bitmap, color=[255, 255, 0])
 
-orange = sly.VideoObject(orange_obj_cls)
-kiwi = sly.VideoObject(kiwi_obj_cls)
 
 # create project meta with all classes and upload them to server
 project_meta = sly.ProjectMeta(obj_classes=[kiwi_obj_cls, orange_obj_cls])
 api.project.update_meta(project.id, project_meta.to_json())
 
 
-################################    Part 2    ######################################
-####################    create rectangle, mask    #########################
-######################  on video "data/orange_kiwi.mp4"   ##########################
+################################    Part 2    #######################################
+######################  Create object figures on frames. #############################
+################### Create collections of objects and frames.  #######################
 
-# prepare demo video (there are 10 frames in this demo video)
-video_path = "data/orange_kiwi.mp4"
 
 # prepare path with masks
 masks_dir = "data/masks"
@@ -68,6 +72,9 @@ points = [
     [199, 806, 417, 980],
 ]
 
+# create VideoObjects
+orange = sly.VideoObject(orange_obj_cls)
+kiwi = sly.VideoObject(kiwi_obj_cls)
 
 # create frames and figures
 frames = []
@@ -84,22 +91,23 @@ for mask in os.listdir(masks_dir):
     frame = sly.Frame(fr_index, figures=[mask_figure, bbox_figure])
     frames.append(frame)
 
-frame_size, vlength = sly.video.get_image_size_and_frames_count(video_path)
 objects = sly.VideoObjectCollection([kiwi, orange])
 frames = sly.FrameCollection(frames)
 
-# result video annotation
+
+################################    Part 3    #####################################
+###########################  Create video annotation  #############################
+################ and upload annotation to the video on server #####################
+
+frame_size, vlength = sly.video.get_image_size_and_frames_count(video_path)
+
+# create result video annotation
 video_ann = sly.VideoAnnotation(
     img_size=frame_size,
     frames_count=vlength,
     objects=objects,
     frames=frames,
 )
-
-# upload video to the dataset on server
-video_name = sly.fs.get_file_name_with_ext(video_path)
-video_info = api.video.upload_path(dataset.id, video_name, video_path)
-print(f"Video has been sucessfully uploaded, id={video_info.id}")
 
 # upload annotation to the video on server
 api.video.annotation.append(video_info.id, video_ann)
