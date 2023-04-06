7306c9799b3aa130bbaa93c0eba62aa96f24dea0
almaz_macbook <almazgimaev@gmail.com>
2023-04-06 11:55:18 +0300

    fix fr_index ordering

--- src/main.py
+++ src/main.py
@@ -56,22 +56,23 @@ masks_dir = "data/masks"
 
 # prepare rectangle points for 10 demo frames
 points = [
-    [632, 136, 817, 350],
-    [655, 139, 842, 355],
-    [672, 145, 864, 361],
-    [700, 158, 885, 366],
-    [700, 153, 885, 367],
-    [724, 156, 914, 375],
-    [745, 164, 926, 385],
-    [770, 177, 944, 396],
-    [793, 189, 966, 410],
-    [806, 199, 980, 417],
+    [136, 632, 350, 817],
+    [139, 655, 355, 842],
+    [145, 672, 361, 864],
+    [158, 700, 366, 885],
+    [153, 700, 367, 885],
+    [156, 724, 375, 914],
+    [164, 745, 385, 926],
+    [177, 770, 396, 944],
+    [189, 793, 410, 966],
+    [199, 806, 417, 980],
 ]
 
 
 # create frames and figures
-figures = []
-for fr_index, mask in enumerate(os.listdir(masks_dir)):
+frames = []
+for mask in os.listdir(masks_dir):
+    fr_index = int(sly.fs.get_file_name(mask).split("_")[-1])
     mask_path = join(masks_dir, mask)
 
     bitmap = sly.Bitmap.from_path(mask_path)
@@ -81,15 +82,15 @@ for fr_index, mask in enumerate(os.listdir(masks_dir)):
     bbox_figure = sly.VideoFigure(kiwi, bbox, fr_index)
 
     frame = sly.Frame(fr_index, figures=[mask_figure, bbox_figure])
-    figures.append(frame)
+    frames.append(frame)
 
-img_size, vlength = sly.video.get_image_size_and_frames_count(video_path)
+frame_size, vlength = sly.video.get_image_size_and_frames_count(video_path)
 objects = sly.VideoObjectCollection([kiwi, orange])
-frames = sly.FrameCollection(figures)
+frames = sly.FrameCollection(frames)
 
 # result video annotation
 video_ann = sly.VideoAnnotation(
-    img_size=img_size,
+    img_size=frame_size,
     frames_count=vlength,
     objects=objects,
     frames=frames,
